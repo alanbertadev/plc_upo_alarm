9de87fed7e259f9eb60a3bb2d66dbb2625ae2022
Alan Berta <alanbertadev@gmail.com>
2015-12-19 20:52:30 +0000

    Version bump to 1.0 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 files = []
 setup(name = "plc-upo-alarm",
-    version = "0.1",
+    version = "1.0",
     description = "Detect and emit a SNS message to a properly configured AWS account when a EoP intruder is detected",
     author = "alanbertadev",
     author_email = "alanbertadev@gmail.com",
